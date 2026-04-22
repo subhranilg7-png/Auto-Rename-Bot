462940dfe814cf6e376a85a7b953cfc299b93cd0
subhranilg7-png <subhranilg7@gmail.com>
2026-04-22 16:35:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6497757690').split()]
-    FORCE_SUB_CHANNELS = [x for x in os.environ.get('FORCE_SUB_CHANNELS', '').split(',') if x]
+    FORCE_SUB_CHANNELS = [x for x in os.environ.get('FORCE_SUB_CHANNELS', '').split(',') if x]  # Fixed: empty default disables force sub
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001868871195"))
     DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1001868871195"))
     
