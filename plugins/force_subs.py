16e6ea01ccfe4e5a21a0da589fd2da4860254db5
subhranilg7-png <subhranilg7@gmail.com>
2026-05-23 12:00:57 +0530

    Update force_subs.py

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -4,96 +4,22 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from pyrogram.errors import UserNotParticipant
 from config import Config
 
-FORCE_SUB_CHANNELS = Config.FORCE_SUB_CHANNELS
-IMAGE_URL = "https://graph.org/file/a27d85469761da836337c.jpg"
+# DISABLED: Force subscription is turned off
+FORCE_SUB_CHANNELS = []  # Empty list means no force subscription
+
+# The code below is disabled but kept for reference
+# To re-enable, change the above line to: FORCE_SUB_CHANNELS = Config.FORCE_SUB_CHANNELS
 
 async def not_subscribed(_, __, message):
-    for channel in FORCE_SUB_CHANNELS:
-        try:
-            user = await message._client.get_chat_member(channel, message.from_user.id)
-            if user.status in {"kicked", "left"}:
-                return True
-        except UserNotParticipant:
-            return True
+    # Always returns False because force sub is disabled
     return False
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    not_joined_channels = []
-    for channel in FORCE_SUB_CHANNELS:
-        try:
-            user = await client.get_chat_member(channel, message.from_user.id)
-            if user.status in {"kicked", "left"}:
-                not_joined_channels.append(channel)
-        except UserNotParticipant:
-            not_joined_channels.append(channel)
-
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=f"• ᴊᴏɪɴ {channel.capitalize()} •", url=f"https://t.me/{channel}"
-            )
-        ]
-        for channel in not_joined_channels
-    ]
-    buttons.append(
-        [
-            InlineKeyboardButton(
-                text="• ᴊᴏɪɴᴇᴅ •", callback_data="check_subscription"
-            )
-        ]
-    )
-
-    text = "**ʙᴀᴋᴋᴀ!!, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴛᴏ ᴀʟʟ ʀᴇǫᴜɪʀᴇᴅ ᴄʜᴀɴɴᴇʟs, ᴊᴏɪɴ ᴛʜᴇ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟs ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ**"
-    await message.reply_photo(
-        photo=IMAGE_URL,
-        caption=text,
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
+    # This function won't be triggered because not_subscribed always returns False
+    pass
 
 @Client.on_callback_query(filters.regex("check_subscription"))
 async def check_subscription(client, callback_query: CallbackQuery):
-    user_id = callback_query.from_user.id
-    not_joined_channels = []
-
-    for channel in FORCE_SUB_CHANNELS:
-        try:
-            user = await client.get_chat_member(channel, user_id)
-            if user.status in {"kicked", "left"}:
-                not_joined_channels.append(channel)
-        except UserNotParticipant:
-            not_joined_channels.append(channel)
-
-    if not not_joined_channels:
-        new_text = "**ʏᴏᴜ ʜᴀᴠᴇ ᴊᴏɪɴᴇᴅ ᴀʟʟ ᴛʜᴇ ʀᴇǫᴜɪʀᴇᴅ ᴄʜᴀɴɴᴇʟs. ᴛʜᴀɴᴋ ʏᴏᴜ! 😊 /start ɴᴏᴡ**"
-        if callback_query.message.caption != new_text:
-            await callback_query.message.edit_caption(
-                caption=new_text,
-                reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("• ɴᴏᴡ ᴄʟɪᴄᴋ ʜᴇʀᴇ •", callback_data='help')]
-                ])
-            )
-    else:
-        buttons = [
-            [
-                InlineKeyboardButton(
-                    text=f"• ᴊᴏɪɴ {channel.capitalize()} •",
-                    url=f"https://t.me/{channel}",
-                )
-            ]
-            for channel in not_joined_channels
-        ]
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text="• ᴊᴏɪɴᴇᴅ •", callback_data="check_subscription"
-                )
-            ]
-        )
-
-        text = "**ʏᴏᴜ ʜᴀᴠᴇ ᴊᴏɪɴᴇᴅ ᴀʟʟ ᴛʜᴇ ʀᴇǫᴜɪʀᴇᴅ ᴄʜᴀɴɴᴇʟs. ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴛʜᴇ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟs ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ**"
-        if callback_query.message.caption != text:
-            await callback_query.message.edit_caption(
-                caption=text,
-                reply_markup=InlineKeyboardMarkup(buttons)
-            )
+    # Just answer the callback without doing anything
+    await callback_query.answer("Force subscription is disabled", show_alert=False)
